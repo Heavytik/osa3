3b8a7e7a4c9ff6a542c37b23b5281eee6d5ea0ad
Jere Tofferi <jere.tofferi@gmail.com>
2018-09-29 13:42:20 +0300

    tehtavat 3.22 asti

--- index.js
+++ index.js
@@ -1,98 +1,116 @@
-
 const express = require("express");
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const Person = require("./models/person");
 
-morgan.token('content', function getContent (req) {
-  return JSON.stringify(req.body)
-})
+morgan.token("content", function getContent(req) {
+  return JSON.stringify(req.body);
+});
 
 const app = express();
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use(express.static('build'))
+app.use(express.static("build"));
 
 // part to handle logging
-app.use(morgan(':method :url :content :response-time'))
+app.use(morgan(":method :url :content :response-time"));
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Martti Tienari",
-    number: "040-123456",
-    id: 2
-  },
-  {
-    name: "Arto Järvinen",
-    number: "040-123456",
-    id: 3
-  },
-  {
-    name: "Lea Kutvonen",
-    number: "040-123456",
-    id: 4
-  }
-]
-
-app.get("/", (req, res) => {
-
-});
+// database acsess
+const getPersons = () => {};
 
 app.get("/api/persons", (req, res) => {
-  // automaattinen content-type application/json sekä automaattinen stringify()
-  res.json(persons);
+  Person.find({})
+    .then(persons => {
+      return res.json(persons.map(Person.format));
+    })
+    .catch(error => {
+      res.status(404).end();
+    });
 });
 
 app.get("/info", (req, res) => {
-  const personsAmount = persons.length
-  res.send(
-    "<p>Puhelinluettelossa on " + personsAmount + " henkilön tiedot.</p><p>" + Date() +"</p>"
-  )
+  Person.find({}).then(persons => {
+    const personsAmount = persons.length;
+    res.send(
+      "<p>Puhelinluettelossa on " +
+        personsAmount +
+        " henkilön tiedot.</p><p>" +
+        Date() +
+        "</p>"
+    );
+  });
 });
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  if( person ) { // jokainen javascript olio on tosi
-    res.json(person)
-  } else {
-    res.status(404).end() // ilman dataa => end()-metodi.
-  }
-})
+app.get("/api/persons/:id", (req, res) => {
+  Person.findById(req.param.id)
+    .then(person => {
+      return res.json(Person.format(person));
+    })
+    .catch(error => {
+      res.status(404).end();
+    });
+});
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
-})
+app.delete("/api/persons/:id", (req, res) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end();
+    })
+    .catch(error => {
+      res.status(400).send({ error: "malformatted id" });
+    });
+});
 
-app.post('/api/persons', (req, res) => {
-  const body = req.body
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
 
   if (body.name === undefined || body.number === undefined) {
-    return res.status(400).json({error: 'content missing'})
+    return res.status(400).json({ error: "content missing" });
   }
-  if (!persons.every(person => person.name !== body.name )) {
-    return res.status(400).json({error: 'name must be unique'})
-  }
+  /*if (!persons.every(person => person.name !== body.name)) {
+    return res.status(400).json({ error: "name must be unique" });
+  }*/
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  });
+
+  Person.find({ name: body.name }).then(result => {
+    if (result.length > 0) {
+      return res.status(403).json({ error: "name already exist" });
+    } else {
+      person
+        .save()
+        .then(response => {
+          res.json(Person.format(response));
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  });
+});
+
+app.put("/api/persons/:id", (request, response) => {
+  const body = request.body;
 
   const person = {
     name: body.name,
-    number: body.number,
-    id: Math.floor(Math.random() * 1000000)
-  }
+    number: body.number
+  };
 
-  persons = persons.concat(person)
-
-  res.json(persons)
-})
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(Person.format(updatedPerson));
+    })
+    .catch(error => {
+      response.status(400).send({ error: "malformatted id" });
+    });
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
---
+++ models/person.js
@@ -0,0 +1,56 @@
+//import { mongo } from 'mongoose';
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+if ( process.env.NODE_ENV !== 'production' ) {
+    require('dotenv').config()
+  }
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url);
+
+const personSchema = new Schema({
+  name: String,
+  number: String
+});
+
+personSchema.statics.format = person => {
+  return {
+    name: person.name,
+    number: person.number,
+    id: person._id
+  };
+};
+
+const Person = mongoose.model("{Person}", personSchema);
+
+/*
+if(process.argv.length > 2) {
+    const person = new Person({
+    name: process.argv[2],
+    number: process.argv[3]
+    })
+
+    person
+    .save()
+    .then(response => {
+        console.log('lisätään henkilö ' + process.argv[2] + ' numero ' + process.argv[3] + ' luetteloon')
+        mongoose.connection.close()
+    })
+} else {
+    Person
+    .find({})
+    .then(result => {
+        console.log('puhelinluettelo:')
+        result.forEach(person => {
+        console.log(person)
+        })
+        mongoose.connection.close()
+    })
+
+}
+*/
+
+module.exports = Person;
